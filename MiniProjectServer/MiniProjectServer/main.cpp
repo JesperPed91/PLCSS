08cfccd429dffd1c5b1cc231b933b8270c959c1f
JesperPed91 <33930662+JesperPed91@users.noreply.github.com>
2017-12-20 15:47:48 +0100

    Add files via upload

---
+++ MiniProjectServer/MiniProjectServer/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std; 
+
+void man()
+{
+	//winsock initialization 
+
+
+
+
+	//creation of socket
+
+	//binding the socket to an ip. and port
+
+	//WS2 the socket is to listen to 
+
+	//waiting for connection 
+
+	//Close listening socket
+
+	//accept and echo message back
+
+	//close the socket
+
+
+
+	//shutdown for Ws2(winsock)
+
+
+}
