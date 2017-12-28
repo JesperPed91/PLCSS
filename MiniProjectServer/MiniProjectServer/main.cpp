44e9262103b004c526d03611fab8094236796b8e
earend16 <28116546+earend16@users.noreply.github.com>
2017-12-28 16:38:08 +0100

    Update main.cpp

--- MiniProjectServer/MiniProjectServer/main.cpp
+++ MiniProjectServer/MiniProjectServer/main.cpp
@@ -7,12 +7,25 @@ using namespace std;
 
 void man()
 {
-	//winsock initialization 
+	//Initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
 
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl; //if we can't create winsock
+		return;
+	}
 
+	// Create a socket (just a number)
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		return;
+	}
 
-
-	//creation of socket
 
 	//binding the socket to an ip. and port
 
