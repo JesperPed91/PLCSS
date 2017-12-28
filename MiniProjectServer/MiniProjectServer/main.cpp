759758805feb818a08595a204094520f3e76fde9
fmydga <34714565+fmydga@users.noreply.github.com>
2017-12-28 19:56:05 +0100

    Update main.cpp

--- MiniProjectServer/MiniProjectServer/main.cpp
+++ MiniProjectServer/MiniProjectServer/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <WS2tcpip.h>
+#include <sstream>
 
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std; 
 
-void man()
+void main()
 {
 	//Initialize winsock
 	WSADATA wsData;
 	WORD ver = MAKEWORD(2, 2);
 
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0)
+	int WinSock = WSAStartup(ver, &wsData);
+	if (WinSock != 0)
 	{
 		cerr << "Can't Initialize winsock! Quitting" << endl; //if we can't create winsock
 		return;
@@ -26,10 +27,45 @@ void man()
 		return;
 	}
 
+	//bind ip address and port to socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // could also use inet_pton ....
 
-	//binding the socket to an ip. and port
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-	//WS2 the socket is to listen to 
+	//tell winsock the socket is for listening
+	listen(listening, SOMAXCONN);
+
+	fd_set master;
+	FD_ZERO(&master);
+
+	FD_SET(listening, &master);
+
+	while (true)
+	{
+		fd_set copy = master;
+
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+		
+		for (int i = 0; i < socketCount; i++)
+		{
+			SOCKET sock = copy.fd_array[i];
+			if (sock == listening)
+			{
+				//accept new connection
+				SOCKET client = accept(listening, nullptr, nullptr);
+
+				//add new connection to the list of connected clients
+				FD_SET(client, &master);
+				//send welcome message to the connected client
+				string welcomeMsg = "Welcome maddafkkas!\r\n"; //Perhaps change this
+				send(client, welcomeMsg.c_str(), welcomeMsg.size() + 1, 0);
+				
+			}//else statement starts here:
+	
+
 
 	//waiting for connection 
 
